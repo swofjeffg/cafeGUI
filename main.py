305bfb16e02396c8a3223bec3e67d839733ff500
Swof <ivank1401@gmail.com>
2022-06-02 13:13:37 +1200

    added working csv reader

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Cafe GUI for practise project
 from tkinter import *
 import pathlib
+import csv
 
 PATH = str(pathlib.Path(__file__).parent.resolve())+'/'
 
@@ -8,21 +9,22 @@ class App():
     def __init__(self, parent):
         self.parent = parent
         
-        self.external_file = External_data_manager.text_file_manager
-        self.external_images = External_data_manager.image_manager
+        self.external_file = External_data_manager.text_file_manager()
+        self.external_images = External_data_manager.image_manager()
+
 
 class External_data_manager():
-    def __init__(self):
-        self.img_source = PATH+'imgs/'
-        self.txt_soruce = PATH+'data/'
-    
-    def text_file_manager(self):
+    def text_file_manager():
         data = []
+        with open(PATH+'data/data.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data.append(row)
         return data
     
-    def image_manager(self):
+    def image_manager():
         images = []
-        for image in self.source:
+        for image in PATH+'imgs/':
             PhotoImage(image)
         return images
 
