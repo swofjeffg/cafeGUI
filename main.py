09688d579a5c3b57d2475a327d5b730a834d81e7
Swof <ivank1401@gmail.com>
2022-06-09 00:33:02 +1200

    partially completed ui

--- main.py
+++ main.py
@@ -16,12 +16,23 @@ class App:
         self.image_data = external_data.image_data
         self.logo = PhotoImage(file=PATH+'imgs/placeholder.png')
         
+        self.categories = []
+        _ = []
+        for entry in self.menu:
+            if entry['category'] not in _:
+                self.categories.append({'category': entry['category'], 'img': entry['img']})
+                _.append(entry['category'])
+        self.categories
+        self.category = self.categories[0]['category']
+        
+        self.order = []
+        
         self.widgets()
         self.state_picker()
     
     def widgets(self):
         master_frame = Frame(self.parent, background=BACKGROUND_COLOUR)
-        master_frame.pack(fill='both')
+        master_frame.pack(fill='both', expand=True)
         
         self.nav_frame = Frame(master_frame, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=2)
         
@@ -43,19 +54,60 @@ class App:
         self.button1.pack()
         self.button2.pack()
         
-        self.nav_frame.pack(side=TOP, expand=True, fill='x')
+        self.nav_frame.pack(side='top', fill='x')
         
         self.state_frame = Frame(master_frame, background=BACKGROUND_COLOUR)
-        self.state_frame.pack()
     
     def initial_state(self):
         root = self.state_frame
+        
+        button_frame_1 = Frame(root, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
+        button_frame_2 = Frame(root, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
+        
+        Button(button_frame_1, text='Click to start!', fg=ORANGE_COLOUR, font='Arial 100', borderwidth=0, background=BACKGROUND_COLOUR, activebackground='#333333', activeforeground=ORANGE_COLOUR, command=lambda:self.state_picker(1)).pack()
+        Button(button_frame_2, text='Restart order', fg=ORANGE_COLOUR, font='Arial 100', borderwidth=0, background=BACKGROUND_COLOUR, activebackground='#333333', activeforeground=ORANGE_COLOUR, command=lambda:self.clear_order()).pack()
+        
+        button_frame_1.pack(pady=30)
+        button_frame_2.pack(pady=30)
+        
+        root.place(anchor='n', relx=.5, rely=.128, relheight=0.872, relwidth=1)
     
     def menu_state(self):
         root = self.state_frame
+        
+        categories_frame = Frame(root, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=2)
+        
+        for category in self.categories:
+            catergory_name = category['category'].capitalize()
+            border = Frame(categories_frame, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
+            frame = Frame(border, background=BACKGROUND_COLOUR)
+            for data in self.image_data:
+                if data['img_name'] == category['img']:
+                    image_data = data['img_file']
+            image = Button(frame, image=image_data, borderwidth=0, background=BACKGROUND_COLOUR, activebackground=BACKGROUND_COLOUR, command=lambda i=category['category']:self.set_category(i))
+            image.img = image_data
+            image.pack()
+            Button(frame, text=catergory_name, fg=ORANGE_COLOUR, borderwidth=0, background=BACKGROUND_COLOUR, activebackground=BACKGROUND_COLOUR, activeforeground=ORANGE_COLOUR, command=lambda i=category['category']:self.set_category(i)).pack()
+            frame.pack(pady=5, padx=5)
+            border.pack(pady=10, padx=10)
+        
+        categories_frame.pack(side='left', fill='y')
+        
+        options_frame = Frame(root, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=2)
+        
+        for entry in self.menu:
+            if entry['category'] == self.category:
+                t = entry['name']
+                Label(options_frame, text=t).pack()
+        
+        options_frame.pack(fill='both', expand=True)
+        
+        root.place(anchor='n', relx=.5, rely=.128, relheight=0.872, relwidth=1)
     
     def final_state(self):
         root = self.state_frame
+        
+        root.place(anchor='n', relx=.5, rely=.128, relheight=0.872, relwidth=1)
 
     def state_picker(self, target_state = 0):
         if self.state_frame.winfo_children():
@@ -74,6 +126,13 @@ class App:
             self.button1.config(text = 'Main menu', command=lambda:self.state_picker(0))
             self.button2.config(text = 'Food menu', command=lambda:self.state_picker(1))
             self.final_state()
+    
+    def clear_order(self):
+        self.order = []
+    
+    def set_category(self, category):
+        self.category = category
+        self.state_picker(1)
 
 class External_data_manager:
     def __init__(self):
@@ -86,13 +145,13 @@ class External_data_manager:
                 pic = PhotoImage(file=PATH+'imgs/'+entry['img'])
             except:
                 pic = PhotoImage(file=PATH+'imgs/placeholder.png')
-            self.image_data.append(pic)
+            self.image_data.append({'img_name': entry['img'],'img_file': pic})
 
 if __name__ == '__main__':
     root = Tk()
     root.title('Cafe Interferance')
-    root.maxsize(1250, 800)
-    root.minsize(1250, 800)
+    root.maxsize(1250, 1000)
+    root.minsize(1250, 1000)
     root.configure(background=BACKGROUND_COLOUR)
     App(root)
     root.mainloop()
