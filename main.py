70ea1e0507c8b5ceb00c2da3b09b8e410ff73d2d
Swof <ivank1401@gmail.com>
2022-06-08 22:46:52 +1200

    started ui finalised navbar backend

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pathlib  # pip install pathlib
 import json
 
 PATH = str(pathlib.Path(__file__).parent.resolve())+'/'
+BACKGROUND_COLOUR = '#111111'
+ORANGE_COLOUR = '#ffa500'
 
 class App:
     def __init__(self, parent):
@@ -12,31 +14,48 @@ class App:
         
         self.menu = external_data.json_data
         self.image_data = external_data.image_data
+        self.logo = PhotoImage(file=PATH+'imgs/placeholder.png')
         
         self.widgets()
+        self.state_picker()
     
     def widgets(self):
-        master_frame = Frame(self.parent)
-        master_frame.place(relx=.5, rely=.5, anchor=CENTER)
+        master_frame = Frame(self.parent, background=BACKGROUND_COLOUR)
+        master_frame.pack(fill='both')
         
-        test = Label(master_frame, image = self.image_data[0])
-        test.image = self.image_data[0]
-        test.pack()
+        self.nav_frame = Frame(master_frame, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=2)
         
-        self.nav_frame = Frame(master_frame)
-        self.nav_frame.pack()
+        logo = Label(self.nav_frame, image=self.logo, background=BACKGROUND_COLOUR)
+        logo.img = self.logo
+        logo.pack(side=LEFT, padx=10, pady=10)
         
-        self.state_frame = Frame(master_frame)
+        Label(self.nav_frame, text='Ormiston Cafe', fg=ORANGE_COLOUR, font='Arial 50', background=BACKGROUND_COLOUR).pack(side=LEFT, padx=100)
+        
+        button_frame_1 = Frame(self.nav_frame, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
+        button_frame_2 = Frame(self.nav_frame, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
+        
+        button_frame_2.pack(side=RIGHT, padx=10)
+        button_frame_1.pack(side=RIGHT, padx=10)
+        
+        self.button1 = Button(button_frame_1, text='Food menu', fg=ORANGE_COLOUR, font='Arial 20', borderwidth=0, background=BACKGROUND_COLOUR, activebackground='#333333', activeforeground=ORANGE_COLOUR, command=lambda:self.state_picker(1))
+        self.button2 = Button(button_frame_2, text='View order', fg=ORANGE_COLOUR, font='Arial 20', borderwidth=0, background=BACKGROUND_COLOUR, activebackground='#333333', activeforeground=ORANGE_COLOUR, command=lambda:self.state_picker(2))
+        
+        self.button1.pack()
+        self.button2.pack()
+        
+        self.nav_frame.pack(side=TOP, expand=True, fill='x')
+        
+        self.state_frame = Frame(master_frame, background=BACKGROUND_COLOUR)
         self.state_frame.pack()
     
     def initial_state(self):
-        pass
+        root = self.state_frame
     
-    def menu(self):
-        pass
+    def menu_state(self):
+        root = self.state_frame
     
     def final_state(self):
-        pass
+        root = self.state_frame
 
     def state_picker(self, target_state = 0):
         if self.state_frame.winfo_children():
@@ -44,10 +63,16 @@ class App:
                 child.destroy()
         
         if target_state == 0:
+            self.button1.config(text = 'Food menu', command=lambda:self.state_picker(1))
+            self.button2.config(text = 'View order', command=lambda:self.state_picker(2))
             self.initial_state()
         if target_state == 1:
-            self.menu()
+            self.button1.config(text = 'Main menu', command=lambda:self.state_picker(0))
+            self.button2.config(text = 'View order', command=lambda:self.state_picker(2))
+            self.menu_state()
         if target_state == 2:
+            self.button1.config(text = 'Main menu', command=lambda:self.state_picker(0))
+            self.button2.config(text = 'Food menu', command=lambda:self.state_picker(1))
             self.final_state()
 
 class External_data_manager:
@@ -66,7 +91,8 @@ class External_data_manager:
 if __name__ == '__main__':
     root = Tk()
     root.title('Cafe Interferance')
+    root.maxsize(1250, 800)
+    root.minsize(1250, 800)
+    root.configure(background=BACKGROUND_COLOUR)
     App(root)
-    root.update()
-    root.minsize(root.winfo_width(), root.winfo_height())
     root.mainloop()
