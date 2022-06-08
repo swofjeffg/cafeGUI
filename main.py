7626d6c3f3dfca43dfbb265ed4abf74236b7a1d3
Swof <ivank1401@gmail.com>
2022-06-08 21:40:30 +1200

    converted data to json and fixed images

--- main.py
+++ main.py
@@ -1,32 +1,67 @@
 # Cafe GUI for practise project
 from tkinter import *
-import pathlib
-import csv
+import pathlib  # pip install pathlib
+import json
 
 PATH = str(pathlib.Path(__file__).parent.resolve())+'/'
 
-class App():
+class App:
     def __init__(self, parent):
         self.parent = parent
+        external_data = External_data_manager()
         
-        self.external_file = External_data_manager.text_file_manager()
-        self.external_images = External_data_manager.image_manager()
-
-
-class External_data_manager():
-    def text_file_manager():
-        data = []
-        with open(PATH+'data/data.csv', 'r') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                data.append(row)
-        return data
+        self.menu = external_data.json_data
+        self.image_data = external_data.image_data
+        
+        self.widgets()
     
-    def image_manager():
-        images = []
-        for image in PATH+'imgs/':
-            PhotoImage(image)
-        return images
+    def widgets(self):
+        master_frame = Frame(self.parent)
+        master_frame.place(relx=.5, rely=.5, anchor=CENTER)
+        
+        test = Label(master_frame, image = self.image_data[0])
+        test.image = self.image_data[0]
+        test.pack()
+        
+        self.nav_frame = Frame(master_frame)
+        self.nav_frame.pack()
+        
+        self.state_frame = Frame(master_frame)
+        self.state_frame.pack()
+    
+    def initial_state(self):
+        pass
+    
+    def menu(self):
+        pass
+    
+    def final_state(self):
+        pass
+
+    def state_picker(self, target_state = 0):
+        if self.state_frame.winfo_children():
+            for child in self.state_frame.winfo_children():
+                child.destroy()
+        
+        if target_state == 0:
+            self.initial_state()
+        if target_state == 1:
+            self.menu()
+        if target_state == 2:
+            self.final_state()
+
+class External_data_manager:
+    def __init__(self):
+        with open(PATH+'data/data.json') as json_file:
+            self.json_data = json.load(json_file)
+  
+        self.image_data = []
+        for entry in self.json_data:
+            try:
+                pic = PhotoImage(file=PATH+'imgs/'+entry['img'])
+            except:
+                pic = PhotoImage(file=PATH+'imgs/placeholder.png')
+            self.image_data.append(pic)
 
 if __name__ == '__main__':
     root = Tk()
