4d60159606c80da0f37cbb67c5ee23eccf738269
Swof <ivank1401@gmail.com>
2022-06-14 14:23:45 +1200

    converting from tkinter to customtkinter

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 from tkinter import *
 import pathlib  # pip install pathlib
 import json
+import customtkinter    # pip install customtkinter
 from PIL import Image, ImageTk  # pip install pillow
 
 PATH = str(pathlib.Path(__file__).parent.resolve())+'/'
-BACKGROUND_COLOUR = '#111111'
-ORANGE_COLOUR = '#ffa500'
+
+DARK_CREAM = '#F2DDC3'
+CREAM = '#FFFFF0'
+BROWN = '#A79280'
 
 class App:
     def __init__(self, parent):
@@ -33,86 +36,74 @@ class App:
         self.state_picker()
     
     def widgets(self):
-        master_frame = Frame(self.parent, background=BACKGROUND_COLOUR)
+        master_frame = Frame(self.parent, background=CREAM)
         master_frame.pack(fill='both', expand=True)
         
-        self.nav_frame = Frame(master_frame, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=2)
+        self.nav_frame = customtkinter.CTkFrame(master_frame, fg_color=DARK_CREAM, corner_radius=0)
         
-        logo = Label(self.nav_frame, image=self.logo, background=BACKGROUND_COLOUR)
+        logo = Label(self.nav_frame, image=self.logo, background=CREAM)
         logo.img = self.logo
         logo.pack(side=LEFT, padx=10, pady=10)
         
-        Label(self.nav_frame, text='Ormiston Cafe', fg=ORANGE_COLOUR, font='Arial 50', background=BACKGROUND_COLOUR).pack(side=LEFT, padx=100)
+        customtkinter.CTkLabel(self.nav_frame, text='Ormiston Cafe', bg_color=DARK_CREAM, text_color=BROWN, text_font=('Arial', 33)).pack(side=LEFT)
         
-        button_frame_1 = Frame(self.nav_frame, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
-        button_frame_2 = Frame(self.nav_frame, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
+        self.button1 = customtkinter.CTkButton(self.nav_frame, fg_color=CREAM, text_color=BROWN, text_font=('Arial', 25), hover=False, corner_radius=30, command=lambda:self.state_picker(1))
+        self.button2 = customtkinter.CTkButton(self.nav_frame, fg_color=CREAM, text_color=BROWN, text_font=('Arial', 25), hover=False, corner_radius=30, command=lambda:self.state_picker(2))
         
-        button_frame_2.pack(side=RIGHT, padx=10)
-        button_frame_1.pack(side=RIGHT, padx=10)
-        
-        self.button1 = Button(button_frame_1, text='Food menu', fg=ORANGE_COLOUR, font='Arial 20', borderwidth=0, background=BACKGROUND_COLOUR, activebackground='#333333', activeforeground=ORANGE_COLOUR, command=lambda:self.state_picker(1))
-        self.button2 = Button(button_frame_2, text='View order', fg=ORANGE_COLOUR, font='Arial 20', borderwidth=0, background=BACKGROUND_COLOUR, activebackground='#333333', activeforeground=ORANGE_COLOUR, command=lambda:self.state_picker(2))
-        
-        self.button1.pack()
-        self.button2.pack()
+        self.button2.pack(side=RIGHT, padx=10)
+        self.button1.pack(side=RIGHT, padx=10)
         
         self.nav_frame.pack(side='top', fill='x')
         
-        self.state_frame = Frame(master_frame, background=BACKGROUND_COLOUR)
+        self.state_frame = Frame(master_frame, background=CREAM)
     
     def initial_state(self):
         root = self.state_frame
         
-        button_frame_1 = Frame(root, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
-        button_frame_2 = Frame(root, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
+        button1 = customtkinter.CTkButton(root, text='Start your\norder', fg_color=DARK_CREAM, text_color=BROWN, text_font=('Arial', 80), hover=False, corner_radius=70, border_width=5, border_color=BROWN, width=700, command=lambda:self.state_picker(1))
+        button2 = customtkinter.CTkButton(root, text='Restart your\norder', fg_color=DARK_CREAM, text_color=BROWN, text_font=('Arial', 80), hover=False, corner_radius=70, border_width=5, border_color=BROWN, width=700, command=lambda:self.clear_order())
         
-        Button(button_frame_1, text='Click to start!', fg=ORANGE_COLOUR, font='Arial 100', borderwidth=0, background=BACKGROUND_COLOUR, activebackground='#333333', activeforeground=ORANGE_COLOUR, command=lambda:self.state_picker(1)).pack()
-        Button(button_frame_2, text='Restart order', fg=ORANGE_COLOUR, font='Arial 100', borderwidth=0, background=BACKGROUND_COLOUR, activebackground='#333333', activeforeground=ORANGE_COLOUR, command=lambda:self.clear_order()).pack()
-        
-        button_frame_1.pack(pady=30)
-        button_frame_2.pack(pady=30)
-        
+        button1.pack(ipady=50, pady=30, padx=50)
+        button2.pack(ipady=50, pady=30, padx=50)
         root.place(anchor='n', relx=.5, rely=.128, relheight=0.872, relwidth=1)
     
     def menu_state(self):
         root = self.state_frame
         
-        categories_frame = Frame(root, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=2)
-        
+        categories_frame = Frame(root, background=CREAM, highlightbackground=BROWN, highlightthickness=2)
         for category in self.categories:
             catergory_name = category['category'].capitalize()
-            border = Frame(categories_frame, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
-            frame = Frame(border, background=BACKGROUND_COLOUR)
+            border = Frame(categories_frame, background=CREAM, highlightbackground=BROWN, highlightthickness=1)
+            frame = Frame(border, background=CREAM)
             for data in self.small_image_data:
                 if data['img_name'] == category['img']:
                     image_data = data['img_file']
-            button = Button(frame, image=image_data, width=120, height=100, borderwidth=0, background=BACKGROUND_COLOUR, activebackground=BACKGROUND_COLOUR, command=lambda i=category['category']:self.set_category(i))
+            button = Button(frame, image=image_data, width=120, height=100, borderwidth=0, background=CREAM, activebackground=CREAM, command=lambda i=category['category']:self.set_category(i))
             button.pack()
-            Button(frame, text=catergory_name, fg=ORANGE_COLOUR, borderwidth=0, background=BACKGROUND_COLOUR, activebackground=BACKGROUND_COLOUR, activeforeground=ORANGE_COLOUR, command=lambda i=category['category']:self.set_category(i)).pack()
-            frame.pack(pady=5, padx=5)
+            Button(frame, text=catergory_name, fg=ORANGE_COLOUR, borderwidth=0, background=CREAM, activebackground=CREAM, activeforeground=ORANGE_COLOUR, command=lambda i=category['category']:self.set_category(i)).pack()
             border.pack(pady=10, padx=10)
         
         categories_frame.pack(side='left', fill='y')
         
-        options_frame = Frame(root, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=2)
+        options_frame = Frame(root, background=CREAM, highlightbackground=BROWN, highlightthickness=2)
         
         x = y = 1
         for entry in self.menu:
             if entry['category'] == self.category:
                 x += 1
                 name, price = entry['name'], entry['price']
-                frame = Frame(options_frame, background=BACKGROUND_COLOUR, highlightbackground='#ffffff', highlightthickness=1)
+                frame = Frame(options_frame, background=CREAM, highlightbackground=BROWN, highlightthickness=1)
                 frame.grid(column = x, row = y, pady=10, padx=10)
                 for data in self.image_data:
                     if data['img_name'] == entry['img']:
                         image_data = data['img_file']
-                image = Label(frame, image=image_data, width=350, height=200, background=BACKGROUND_COLOUR)
+                image = Label(frame, image=image_data, width=350, height=200, background=CREAM)
                 image.pack()
                 try:
                     size = entry['size']
-                    Label(frame, text=f'{name.capitalize()}({size}) for ${price}', fg=ORANGE_COLOUR, background=BACKGROUND_COLOUR).pack()
+                    Label(frame, text=f'{name.capitalize()}({size}) for ${price}', fg=ORANGE_COLOUR, background=CREAM).pack()
                 except KeyError:
-                    Label(frame, text=f'{name.capitalize()} for ${price}', fg=ORANGE_COLOUR, background=BACKGROUND_COLOUR).pack()
+                    Label(frame, text=f'{name.capitalize()} for ${price}', fg=ORANGE_COLOUR, background=CREAM).pack()
                 if x == 3:
                     x = 1
                     y += 1
@@ -134,16 +125,16 @@ class App:
                 child.destroy()
         
         if target_state == 0:
-            self.button1.config(text = 'Food menu', command=lambda:self.state_picker(1))
-            self.button2.config(text = 'View order', command=lambda:self.state_picker(2))
+            self.button1.config(text = 'Food\nMenu', command=lambda:self.state_picker(1))
+            self.button2.config(text = 'View\nOrder', command=lambda:self.state_picker(2))
             self.initial_state()
         if target_state == 1:
-            self.button1.config(text = 'Main menu', command=lambda:self.state_picker(0))
-            self.button2.config(text = 'View order', command=lambda:self.state_picker(2))
+            self.button1.config(text = 'Main\nMenu', command=lambda:self.state_picker(0))
+            self.button2.config(text = 'View\nOrder', command=lambda:self.state_picker(2))
             self.menu_state()
         if target_state == 2:
-            self.button1.config(text = 'Main menu', command=lambda:self.state_picker(0))
-            self.button2.config(text = 'Food menu', command=lambda:self.state_picker(1))
+            self.button1.config(text = 'Main\nMenu', command=lambda:self.state_picker(0))
+            self.button2.config(text = 'Food\nMenu', command=lambda:self.state_picker(1))
             self.final_state()
     
     def clear_order(self):
@@ -203,8 +194,8 @@ class External_data_manager:
 if __name__ == '__main__':
     root = Tk()
     root.title('Cafe Interferance')
-    root.maxsize(1250, 1000)
-    root.minsize(1250, 1000)
-    root.configure(background=BACKGROUND_COLOUR)
+    root.maxsize(700, 1000)
+    root.minsize(700, 1000)
+    root.configure(background=BROWN)
     App(root)
     root.mainloop()
